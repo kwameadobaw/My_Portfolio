fff1ce70f6ad0998d23d0a865fb9ea0fcedc1c96
kwameadobaw <kwame.adobawjnr@gmail.com>
2024-06-29 16:01:11 +0000

    new update

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleSwitch = document.getElementById('dark-mode-toggle');
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+
+        if (currentTheme === 'dark-mode') {
+            toggleSwitch.checked = true;
+        }
+    }
+
+    toggleSwitch.addEventListener('change', () => {
+        if (toggleSwitch.checked) {
+            document.body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+            localStorage.setItem('theme', 'light-mode');
+        }
+    });
+});
